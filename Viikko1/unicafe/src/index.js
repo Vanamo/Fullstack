8535b29f1e57dcdeffea5cf59cba3a4a3a03bd25
Piirainen Vanamo K <vseppane@da5-cs-bk107-24.pc.helsinki.fi>
2018-01-18 16:42:56 +0200

    1.6

---
+++ Viikko1/unicafe/src/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {
+            hyva: 0,
+            neutraali: 0,
+            huono: 0
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Anna palautetta</h1>
+
+                <button onClick={() => this.setState({ hyva: this.state.hyva + 1 })}>
+                    hyvä
+                </button>
+                <button onClick={() => this.setState({ neutraali: this.state.neutraali + 1 })}>
+                    neutraali
+                </button>
+                <button onClick={() => this.setState({ huono: this.state.huono + 1 })}>
+                    huono
+                </button>  
+
+                <h1>Statistiikka</h1>
+
+                <p>hyvä {this.state.hyva}</p>
+                <p>neutraali {this.state.neutraali}</p>
+                <p>huono {this.state.huono}</p>
+            </div>              
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)

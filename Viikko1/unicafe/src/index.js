0bb1ca0ec2c6a656517ec43034d8fd40886b64d1
Piirainen Vanamo K <vseppane@da5-cs-bk107-26.pc.helsinki.fi>
2018-01-19 14:57:11 +0200

    1.10

--- Viikko1/unicafe/src/index.js
+++ Viikko1/unicafe/src/index.js
@@ -12,29 +12,141 @@ class App extends React.Component {
         }
     }
 
-    render() {
+    keskiarvo = () => {
+        return (
+            (this.state.hyva - this.state.huono) / (this.state.hyva 
+            + this.state.neutraali + this.state.huono)
+        )
+    }
+
+    positiivisia = () => {
+        return (
+            this.state.hyva / (this.state.hyva 
+                + this.state.neutraali + this.state.huono) * 100
+            )
+    }
+
+    klikHyva = () => {
+        this.setState({ 
+            hyva: this.state.hyva + 1
+        })    
+    }
+
+    klikNeutraali = () => {
+        this.setState({ 
+            neutraali: this.state.neutraali + 1
+        })    
+    }
+
+    klikHuono = () => {
+        this.setState({ 
+            huono: this.state.huono + 1
+        })    
+    }
+
+    asetaArvoon = (palaute) => () => {
+        if (palaute === 'hyva') {
+            this.setState({hyva: this.state.hyva + 1})
+        }  else if (palaute === 'neutraali') {
+            this.setState({neutraali: this.state.neutraali + 1})
+        } else if (palaute === 'huono') {
+            this.setState({huono: this.state.huono + 1})
+        }
+    }
+
+    render() {   
         return (
             <div>
                 <h1>Anna palautetta</h1>
 
-                <button onClick={() => this.setState({ hyva: this.state.hyva + 1 })}>
-                    hyvä
-                </button>
-                <button onClick={() => this.setState({ neutraali: this.state.neutraali + 1 })}>
-                    neutraali
-                </button>
-                <button onClick={() => this.setState({ huono: this.state.huono + 1 })}>
-                    huono
-                </button>  
+                <Button 
+                    funktio={this.asetaArvoon}
+                    palaute={'hyva'}
+                    text="hyvä"
+                />
+                <Button 
+                    funktio={this.asetaArvoon}
+                    palaute={'neutraali'}
+                    text="neutraali"
+                />
+                <Button 
+                    funktio={this.asetaArvoon}
+                    palaute={'huono'}
+                    text="huono"
+                />
 
-                <h1>Statistiikka</h1>
-
-                <p>hyvä {this.state.hyva}</p>
-                <p>neutraali {this.state.neutraali}</p>
-                <p>huono {this.state.huono}</p>
-            </div>              
+                <Statistics 
+                    state={this.state}
+                    keskiarvo={this.keskiarvo()}
+                    positiivisia={this.positiivisia()}
+                />
+            </div>           
         )
     }
+}
+
+const Button = (props) => {
+    const {funktio, palaute, text} = props
+    return (
+        <button onClick={funktio(palaute)}>
+            {text}
+        </button>
+    )        
+}
+
+const Statistic = (props) => {
+    const {text, state} = props
+    if (text === 'positiivisia') {
+        return (
+            <div>
+                <p>{text} {state} %</p>
+            </div>
+        )
+    }
+    return (
+        <div>
+            <p>{text} {state}</p>
+        </div>
+    )
+}
+
+const Statistics = (props) => {
+    const {state, keskiarvo, positiivisia} = props
+    if (state.hyva === 0 && state.neutraali === 0 && 
+        state.huono === 0) {
+            return (
+                <div>                
+                    <h1>Statistiikka</h1>
+                    <p>Ei yhtään palautetta annettu</p>
+                </div>
+            )
+        }
+    return (
+        <div>
+            <h1>Statistiikka</h1>
+
+            <Statistic
+                text="hyvä"
+                state={state.hyva}
+            />
+            <Statistic
+                text="neutraali"
+                state={state.neutraali}
+            />
+            <Statistic
+                text="huono"
+                state={state.huono}
+            />
+            <Statistic
+                text="keskiarvo"
+                state={keskiarvo}
+            />
+            <Statistic
+                text="positiivisia"
+                state={positiivisia}
+            />  
+        </div>
+    )
 }
 
 ReactDOM.render(
